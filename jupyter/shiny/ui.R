8a69502bce53d083216f1bd3d463d6e47b264b65
Franz Osorio <f.osorio@zbw.eu>
2020-03-02 09:45:26 +0100

    Update ui.R

--- jupyter/shiny/ui.R
+++ jupyter/shiny/ui.R
@@ -1,32 +1,18 @@
-library(shiny)
-
-# Define UI for app that draws a histogram ----
-ui <- fluidPage(
-
-  # App title ----
-  titlePanel("Hello Shiny!"),
-
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
-
-    # Sidebar panel for inputs ----
-    sidebarPanel(
-
-      # Input: Slider for the number of bins ----
-      sliderInput(inputId = "bins",
-                  label = "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-
-    ),
-
-    # Main panel for displaying outputs ----
-    mainPanel(
-
-      # Output: Histogram ----
-      plotOutput(outputId = "distPlot")
-
+fluidPage(
+    titlePanel("Journal Altmetrics"),
+    sidebarLayout(
+        sidebarPanel("Sidebar Panel"),
+        mainPanel(
+            h1('Altmetric Scores'),
+            plotOutput('altScorePlot'),
+            br(),
+            br(),
+            br(),
+            plotOutput('avgScorePlot'),
+            br(),
+            br(),
+            br(),
+            plotOutput('pieChart')
+        )
     )
-  )
 )
