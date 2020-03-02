56fb24b49da92285eab89d12183ac13ff7c6ef18
Franz Osorio <f.osorio@zbw.eu>
2020-03-02 09:44:59 +0100

    Update server.R

--- jupyter/shiny/server.R
+++ jupyter/shiny/server.R
@@ -1,23 +1,44 @@
-# Define server logic required to draw a histogram ----
-server <- function(input, output) {
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(data.table)
+library(plotly)
 
-  # Histogram of the Old Faithful Geyser Data ----
-  # with requested number of bins
-  # This expression that generates a histogram is wrapped in a call
-  # to renderPlot to indicate that:
-  #
-  # 1. It is "reactive" and therefore should be automatically
-  #    re-executed when inputs (input$bins) change
-  # 2. Its output type is a plot
-  output$distPlot <- renderPlot({
 
-    x    <- faithful$waiting
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+df <- read.csv('data/cleaned_altmetrics.csv', header=TRUE, sep=';')
+d <- aggregate(altmetric_score ~ journal_name, df, max)
+a <- aggregate(altmetric_score ~ journal_name, df, mean)
+summary <- setDT(df)[, c(lapply(.SD[, c(10:27), with=FALSE], sum)), by = journal_name]
+sub <- data.frame(subset(summary, journal_name == 'Journal of Finance'))
 
-    hist(x, breaks = bins, col = "#75AADB", border = "white",
-         xlab = "Waiting time to next eruption (in mins)",
-         main = "Histogram of waiting times")
+flipped <- as.data.frame(t(sub))
+flipped <- setDT(flipped, keep.rownames = TRUE)[]
+names(flipped)[1] <- 'key'
+names(flipped)[2] <- 'value'
+# remove first row which has a string
+flipped <- flipped[-1,]
+# make sure there are no strings
+flipped$values <- as.numeric(as.character(flipped$value))
 
+function(input, output){
+    output$altScorePlot <- renderPlot({
+        ggplot(d, aes(x=journal_name, y=altmetric_score)) +
+            geom_bar(stat="identity") +
+            theme(axis.text.x = element_text(angle=45, hjust=1)) +
+            labs(title = "Highest Score", x = "Journal Name", y = "Altmetric Score")
     })
 
+    output$avgScorePlot <- renderPlot({
+        ggplot(a, aes(x=journal_name, y=altmetric_score)) +
+            geom_bar(stat="identity") +
+            theme(axis.text.x = element_text(angle=45, hjust=1)) +
+            labs(title = "Average Score", x = "Journal Name", y = "Altmetric Score")
+    })
+
+    output$pieChart <- renderPlot({
+        plot_ly(flipped, labels=~key, values = ~values, type='pie') %>%
+            layout(title = 'Journal of Finance Engagement',
+                   xaxis = list(showgrid=FALSE, zeroline=FALSE, showticklabels=FALSE),
+                   yaxis = list(showgrid=FALSE, zeroline=FALSE, showticklabels=FALSE))
+    })
 }
